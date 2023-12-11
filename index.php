4d984340667a031f598042210f84ae80c89b83ee
Gabriele-Zini <gabrielezini845@gmail.com>
2023-12-11 15:22:16 +0100

    add: censored output

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-$paragraph = $_GET['paragraph'];
-$censored = $_GET['censored'];
+$paragraph = trim($_GET['paragraph']);
+$censored = trim($_GET['censored']);
 ?>
 
 <!DOCTYPE html>
@@ -10,17 +10,21 @@ $censored = $_GET['censored'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php badwords</title>
-    <link rel="stylesheet" href="./css/">
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 
 <body>
 
-    <form action="request.pho" method="GET">
-        <label for="paragraph">inserisci il tuo paragrafo</label>
-        <input type="text" id="paragraph" name="paragraph">
+    <form action="request.php" method="GET">
+        <div class="form-section form-1">
+            <label for="paragraph">inserisci il tuo paragrafo</label>
+            <textarea type="text" id="paragraph" name="paragraph" rows="4" cols="50"></textarea>
+        </div>
 
-        <label for="censored">inserisci la parola da censurare</label>
-        <input type="text" id="censored" name="censored">
+        <div class="form-section form-2">
+            <label for="censored">inserisci la parola da censurare</label>
+            <input type="text" id="censored" name="censored">
+        </div>
         <button type="submit">invia</button>
     </form>
 
--- request.php
+++ request.php
@@ -1,15 +1,27 @@
 <?php
-
+$paragraph = trim($_GET['paragraph']);
+$censored = trim($_GET['censored']);
+$censored_paragraph = str_replace($censored, "***", $paragraph);
+$censored_paragraph_length = strlen($censored_paragraph);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>php badwords</title>
+    <link rel="stylesheet" href="./css/style.css">
 </head>
+
 <body>
 
+    <div class="first-output">
+        <p> <span>Paragrafo:</span><?php echo $censored_paragraph ?></p>
+        <p><span>Lunghezza del paragrafo</span> <?php echo $censored_paragraph_length ?></p>
+    </div>
+
 </body>
+
 </html>
