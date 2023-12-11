af25ab6e517b0b54180c326b150e4fd26dd95866
Gabriele-Zini <gabrielezini845@gmail.com>
2023-12-11 14:26:17 +0100

    variables init

--- index.php
+++ index.php
@@ -1,14 +1,28 @@
 <?php
+$paragraph = $_GET['paragraph'];
+$censored = $_GET['censored'];
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>php badwords</title>
+    <link rel="stylesheet" href="./css/">
 </head>
+
 <body>
-    
+
+    <form action="request.pho" method="GET">
+        <label for="paragraph">inserisci il tuo paragrafo</label>
+        <input type="text" id="paragraph" name="paragraph">
+
+        <label for="censored">inserisci la parola da censurare</label>
+        <input type="text" id="censored" name="censored">
+    </form>
+
 </body>
+
 </html>
